73b662e8150f2dd8d2be45287606001005a54689
Swapnil Panchal <v-swapnil.panchal@dana.id>
2024-04-13 14:56:58 +0530

    [UPDATE] uploaded assetlinks file on server for android deeplinking.

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const errorHandler = require("./middleware/errorhandler");
 const connectDb = require("./config/dbConnection");
 const dotenv = require("dotenv").config();
 const cors = require("cors");
+const path = require("path");
 
 const corsOption = {
   credentials: true,
@@ -13,9 +14,14 @@ const port = process.env.PORT || 5000;
 const app = express();
 app.use(cors(corsOption));
 app.use(express.json());
+
 app.use("/api/BRT", require("./routes/routes"));
 app.use("/api/", require("./routes/advertisementRoute"));
+
+// Serve the assetlinks.json file statically
+app.use("/.well-known", express.static(path.join(__dirname, ".well-known")));
+
 app.use(errorHandler);
 app.listen(port, () => {
-  console.log("listing",port);
+  console.log("listing", port);
 });
